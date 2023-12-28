bbe2a8ec3ae177cb3a8bc068ba2ec4ff5ca32dd7
Kevin Venâncio <83186522+KevinVenancio@users.noreply.github.com>
2023-12-28 12:16:48 -0300

    Add files via upload

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,42 @@
+export const dataFake = [
+  {
+    "id": '1',
+    "photo":
+      'https://ogimg.infoglobo.com.br/in/22366158-f5d-4b5/FT1086A/74569980_This-image-released-by-Disney-and-Marvel-Studios27-shows-Chadwick-Boseman-in-a-scene-from-Bl.jpg',
+    "title": 'novo filme do pantera negra',
+    "description":
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur deserunt fugit alias voluptates asperiores nihil.',
+  },
+  {
+    "id": '2',
+    "photo":
+      'https://img.olhardigital.com.br/wp-content/uploads/2023/10/suits-serie.jpg',
+    "title": 'nova serie derivada de suits',
+    "description":
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur deserunt fugit alias voluptates asperiores nihil.',
+  },
+  {
+    "id": '3',
+    "photo":
+      'https://ichef.bbci.co.uk/news/640/cpsprodpb/35a5/live/2ff6b8f0-9b63-11ee-b9a7-c91b9dfa91e5.png',
+    "title": 'quem foi a melhor rainha em The crown?',
+    "description":
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur deserunt fugit alias voluptates asperiores nihil.',
+  },
+  {
+    "id": '4',
+    "photo":
+      'https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2023/01/dc-comics-2.png',
+    "title": 'nova animação da DC',
+    "description":
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur deserunt fugit alias voluptates asperiores nihil.',
+  },
+  {
+    "id": '6',
+    "photo":
+      'https://t.ctcdn.com.br/DfpyXvq-9WUrIg59USnMF-WINZU=/19x630:1499x1486/640x360/smart/i617029.jpeg',
+    "title": 'Quando sai a nova temporada de Sandman na Netflix?',
+    "description":
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur deserunt fugit alias voluptates asperiores nihil.',
+  },
+];
